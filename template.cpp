30575e015b13741dadfad408c26887b46b02b581
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-03-26 19:19:38 -0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) Year. Joseph Wondimagnehu. All rights reserved.
+//
+// Created by: Joseph Wondimagnehu
+// Date: Month Day, Year
+/* Add something. */
+
+/* Include the iostream library for
+input and output functionalities. */
+#include <iostream>
+
+// Runs the main function.
+int main() {}
